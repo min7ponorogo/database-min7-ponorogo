3145a987572bff05f1a9495d19c93e91f8d5a099
min7ponorogo <min7ponorogo141197@gmail.com>
2026-01-20 10:22:52 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,10 @@
+import './globals.css' // Baris ini WAJIB ada agar Tailwind aktif
+
+export const metadata = {
+  title: 'Database MIN 7 Ponorogo',
+  description: 'Sistem Informasi Data Siswa',
+}
+
 export default function RootLayout({
   children,
 }: {
